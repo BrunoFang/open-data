29b9c3dd23ab73d4a477cd14b19a49dbe84b0263
Bruno Fang <bruno.fang@ec.gc.ca>
2022-04-08 15:31:08 +0000

    Deployed a53262d with MkDocs version: 1.3.0

---
+++ js/cmip5.js
@@ -0,0 +1,37 @@
+const parser = new DOMParser();
+
+let layers = [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    }),
+    new ol.layer.Image({
+      source: new ol.source.ImageWMS({
+        format: 'image/png',
+        url: 'https://geo.weather.gc.ca/geomet-climate',
+        params: {'LAYERS': 'CMIP5.TT.HISTO.SUMMER.ABS_PCTL50', 'TILED': true},
+        crossOrigin: 'Anonymous'
+      })
+    }),
+  ]
+
+let map = new ol.Map({
+  target: 'map',
+  layers: layers,
+  view: new ol.View({
+    center: ol.proj.fromLonLat([-97, 57]),
+    zoom: 3
+  })
+});
+
+let exportMapFunction = function(e) {
+  map.once('postcompose', function(event) {
+    var canvas = event.context.canvas;
+    canvas.toBlob(function(blob) {
+      saveAs(blob, 'msc-geomet_web-map_export.jpg')
+    }, 'image/jpeg',0.9);
+  });
+  map.renderSync();
+};
+
+let exportButton = document.getElementById('exportmap');
+exportButton.addEventListener('click', exportMapFunction, false);
